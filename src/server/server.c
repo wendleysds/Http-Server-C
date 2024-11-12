31a5f61e36c7968c96954b79ca628831e1bd0ba5
Wendley Santos <wendley.sdsilva@gmail.com>
2024-11-12 18:37:42 -0300

    server lib

--- src/server/server.c
+++ src/server/server.c
@@ -1,6 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/socket.h>
-#include"server.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "server.h"
 
+int init_server(struct Http_Server* http_server, uint16_t &port){
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
 
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock_fd == -1)
+		return -1;
+
+	if(bind(sock_fd, &server_addr, sizeof(server_addr)) == -1)
+		return -2;
+
+	return 0;
+}
+
+int main(){return 0;}
--- src/server/server.h
+++ src/server/server.h
@@ -1,6 +1,14 @@
 #ifndef HTTP_SERVER_H
 #define HTTP_SERVER_H
 
+#include <inttypes.h>
 
+struct Http_Server{
+	uint16_t port;
+	int socket;
+};
+
+void close_server(struct Http_Server*);
+int init_server(struct Http_Server*, uint16_t);
 
 #endif

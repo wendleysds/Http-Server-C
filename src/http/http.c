8b6da380834913c76850c085b4b0c984180f75ec
Wendley Santos <wendley.sdsilva@gmail.com>
2024-12-11 00:10:53 -0300

    added http hearder file

---
+++ src/http/http.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include "http.h"
+
+void init_response(struct HttpResponse* res){
+	res->http_version[0] = '\0';
+	res->status_code = 200;
+	snprintf(res->status_message, sizeof(res->status_message), "OK");
+	res->header_count = 0;
+	res->body = NULL;
+}
+
+void init_request(struct HttpRequest *req){
+	req->method[0] = '\0';
+	req->path[0] = '\0';
+	req->http_version[0] = '\0';
+	req->header_count = 0;
+	req->body = NULL;
+}
+
+void add_header(struct HttpHeader *headers, uint8_t* header_count, const char *name, const char *value){
+	if(*header_count < MAX_HEADERS){
+		snprintf(headers[*header_count].name, MAX_HEADERS_NAME, "%s", name);
+		snprintf(headers[*header_count].value, MAX_HEADERS_VALUE, "%s", value);
+		(*header_count)++;
+	}
+}
+
+void parse_request(char *raw_request, struct HttpRequest *req){
+	init_request(req);
+	char* line = strtok(raw_request, "\r\n");
+	sscanf(line, "%s %s %s", req->method, req->path, req->http_version);
+	while((line = strtok(NULL, "\r\n")) && strlen(line) > 0){
+		char header_name[MAX_HEADERS_NAME], header_value[MAX_HEADERS_VALUE];
+
+		sscanf(line, "%[^:]: %[^\r\n]", header_name, header_value);
+		add_header(req->headers, &req->header_count, header_name, header_value);
+	}
+
+	line = strtok(NULL, "");
+	if(line){
+		req->body = strdup(line);
+	}
+}
+
---
+++ src/http/http.h
@@ -0,0 +1,41 @@
+#ifndef HTTP_H
+#define HTTP_H
+
+#define MAX_HEADERS 20
+#define MAX_HEADERS_NAME 256
+#define MAX_HEADERS_VALUE 1024
+
+#include <inttypes.h>
+
+struct HttpHeader{
+	char name[MAX_HEADERS_NAME];
+	char value[MAX_HEADERS_VALUE];
+};
+
+struct HttpResponse{
+	char http_version[16];
+	uint16_t status_code;
+	char status_message[64];
+	struct HttpHeader headers[MAX_HEADERS];
+	uint8_t header_count;
+	char* body;
+};
+
+struct HttpRequest{
+	char method[16];
+	char path[256];
+	char http_version[16];
+	struct HttpHeader headers[MAX_HEADERS];
+	uint8_t header_count;
+	char* body;
+};
+
+void init_response(struct HttpResponse* res);
+void init_request(struct HttpRequest* req);
+void add_header(struct HttpHeader *headers, uint8_t* header_count, const char *name, const char *value);
+void parse_request(char *raw_request, struct HttpRequest* req);
+
+#endif
+
+
+
